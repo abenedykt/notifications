9956841825b6c0e2be224f1e6d188498652d4d83
Ewelina210891 <ewelina210891@gmail.com>
2013-10-15 15:47:58 +0200

    Rozdzielenie plikow na poszczegolne aplikacje (opis w pliku read me!). Pierwsze podejscie do ChatService.

---
+++ ChatService/WebChatSite/App_Code/Message.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Message
+{
+    public int SenderId { get; set; }
+    public int ReceiverId { get; set; }
+    public string Content { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ ChatService/WebChatSite/App_Code/User.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public class User
+{
+    public int EmployeeId { get; set; }
+    public string EmployeeName { get; set; }
+    public List<string> ConnectionId { get; set; }
+}
