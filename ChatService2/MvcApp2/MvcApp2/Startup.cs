e6c4d794bab366317db30d1dc7ae2c697e9f8397
Ewelina210891 <ewelina210891@gmail.com>
2013-10-29 12:41:16 +0100

    Porzadek w ChatService2

---
+++ ChatService2/MvcApp2/MvcApp2/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MvcApp1.Startup))]
+
+namespace MvcApp1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapHubs();
+        }
+    }
+}
